e599f20128af9a4f862700cfd8709d52fb38cebe
MylesFTOP <m.fullen87@gmail.com>
2019-01-13 16:49:57 +0000

    Core program and class library with validation class.

---
+++ ValidationCore/LuhnCheck/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LuhnLibrary;
+
+namespace LuhnCheck
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Validate Input = new Validate();
+
+            Console.WriteLine("Enter number for validation:");
+            Input.InputString = Console.ReadLine();
+
+            Console.WriteLine();
+        }
+    }
+}
---
+++ ValidationCore/LuhnLibrary/Validate.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LuhnLibrary
+{
+    public class Validate
+    {
+        string inputString = "";
+        int parsedInput = 0;
+        bool parseSucceeded = false;
+        bool validLuhn = false;
+
+        public string InputString
+        {
+            get { return inputString; }
+            set { inputString = value; }
+        }
+
+        public bool ValidInput
+        {
+            get { return parseSucceeded; }
+        }
+
+        public bool ValidLuhn
+        {
+            get { return validLuhn; }
+        }
+
+        private void ParseInputString()
+        {
+            string msg = "";
+
+            // Throw a FormatException if the string does not consist solely of numbers
+            try
+            {
+                if (int.TryParse(inputString, out parsedInput) == false)
+                {
+                    msg = "\nInput must be a number.";
+                    throw new FormatException(msg);
+                }
+                parseSucceeded = true;
+            }
+            catch (FormatException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+
+        private void ValidateInputString()
+        {
+            // Default is modulo 10 version of Luhn for now - possibly extend to modulo n in future
+            if (parseSucceeded)
+            {
+                // Code to follow, first testing that invalid format of input throws exception
+                validLuhn = true;
+            }
+        }
+    }
+}
