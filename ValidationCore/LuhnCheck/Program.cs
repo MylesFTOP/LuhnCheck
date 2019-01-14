caca753b129b406fb6a739b09e95201b62f81b01
MylesFTOP <m.fullen87@gmail.com>
2019-01-14 02:10:19 +0000

    Moving stuff around to try and remove input and validation from main body of Program, and call it in Validate instead.

--- ValidationCore/LuhnCheck/Program.cs
+++ ValidationCore/LuhnCheck/Program.cs
@@ -12,12 +12,21 @@ namespace LuhnCheck
         static void Main(string[] args)
         {
             ParseInput ParseInput = new ParseInput();
+            Validate Validate = new Validate();
 
             Console.WriteLine("Enter number for validation:");
             ParseInput.InputString = Console.ReadLine();
+
+            // Validate.ValidateInputString();
+            // Console.WriteLine($"{ParseInput.ValidInput}");
             ParseInput.ParseInputString();
 
+            Console.WriteLine($"{ParseInput.InputString}");
             Console.WriteLine($"{ParseInput.ValidInput}");
+            Console.WriteLine($"{Validate.ValidLuhn}");
         }
+
+
+
     }
 }
--- ValidationCore/LuhnLibrary/Validate.cs
+++ ValidationCore/LuhnLibrary/Validate.cs
@@ -4,22 +4,32 @@ namespace LuhnLibrary
 {
     public class Validate
     {
+        bool parseSuceeded = false;
         bool validLuhn = false;
-
+        
         public bool ValidLuhn
         {
             get { return validLuhn; }
         }
-        /*
-        private void ValidateInputString()
+
+        public void ParseInputString()
         {
+            ParseInput ParseInput = new ParseInput();
+            ParseInput.ParseInputString();
+            bool parseSuceeded = ParseInput.ValidInput;
+        }
+        
+        public void ValidateInputString()
+        {
+            ParseInputString();
+
             // Default is modulo 10 version of Luhn for now - possibly extend to modulo n in future
-            if ( == true)
+            if (parseSuceeded == true)
             {
                 // Code to follow, first testing that invalid format of input throws exception
                 validLuhn = true;
             }
         }
-        */
+        
     }
 }
