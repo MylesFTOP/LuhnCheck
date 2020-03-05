6670abab4942701b98995d3ef9599b2e355311db
MylesFTOP <m.fullen87@gmail.com>
2020-03-05 16:58:59 +0000

    Tearing down code to start with base functionality in a more simple way

--- ValidationCore/LuhnCheck/Program.cs
+++ ValidationCore/LuhnCheck/Program.cs
@@ -12,11 +12,7 @@ namespace LuhnCheck
     {
         static void Main(string[] args)
         {
-            LuhnValidationCandidate Candidate = new LuhnValidationCandidate();
-            Candidate.inputString = Console.ReadLine();
-            Candidate.ValidInput = true;
-            Candidate.ValidateInputString();
-            Console.WriteLine(Candidate.ValidLuhn);
+            
         }
 
     }
