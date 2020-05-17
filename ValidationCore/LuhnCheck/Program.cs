576a0c430fe8093a09f189f061cd3bb92abf2ca6
MylesFTOP <m.fullen87@gmail.com>
2020-05-18 00:54:45 +0100

    Proof of concept implementation of console application

--- ValidationCore/LuhnCheck/Program.cs
+++ ValidationCore/LuhnCheck/Program.cs
@@ -12,10 +12,29 @@ namespace LuhnCheck
     {
         static void Main(string[] args)
         {
-            // string input = Console.ReadLine();
-            string input = "7992739871";
+            string input = "";
             LuhnValidator luhnValidator = new LuhnValidator();
-            Console.WriteLine(luhnValidator.AddLuhnSuffix(input));
+
+            Console.WriteLine("Generate [n]ew checksum or [v]alidate existing checksum?");
+            string option = Console.ReadLine();
+            switch (option) { 
+                case "N":
+                case "n":
+                    Console.WriteLine("Please enter number requiring new checksum:");
+                    input = Console.ReadLine();
+                    Console.WriteLine("New checksum is " + luhnValidator.AddLuhnSuffix(input));
+                    break;
+                case "V":
+                case "v":
+                    Console.WriteLine("Please enter number requiring checksum validation:");
+                    input = Console.ReadLine();
+                    string result = luhnValidator.CheckLuhnSuffix(input) ? "valid" : "not valid";
+                    Console.WriteLine($"Checksum is {result}.");
+                    break;
+                default:
+                    Console.WriteLine("No valid option selected.");
+                    break;
+            }
         }
     }
 }
