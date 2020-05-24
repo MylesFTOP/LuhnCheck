837730f51d66b37432aba68abbc345e28ba25122
MylesFTOP <m.fullen87@gmail.com>
2020-05-24 15:36:13 +0100

    Added quit option back; changed ValidationType to ValidationOption to receive input

--- ValidationCore/LuhnLibrary/ConsoleApp.cs
+++ ValidationCore/LuhnLibrary/ConsoleApp.cs
@@ -11,56 +11,68 @@ namespace LuhnLibrary
         readonly LuhnValidator luhnValidator = new LuhnValidator();
 
         public void RunApplication() {
-            Console.WriteLine("Please choose an option from the following:\n" +
-                "[N]ew checksum\n" +
-                "[R]eturn check digit\n" +
-                "[V]alidate existing checksum" //\n" +
-                // "[Q]uit"
-            );
-
+            DisplayOptions();
             var option = Console.ReadLine();
-            var validationType = SetValidationType(option);
-            var userMessage = SetUserPrompt(validationType);
+            var validationOption = SetValidationOption(option);
+            var userMessage = SetUserPrompt(validationOption);
             var input = TakeInput(userMessage);
-            RunValidation(validationType, input);
+            RunValidation(validationOption, input);
         }
-        
-        private enum ValidationType
-        {
+
+        private static void DisplayOptions() {
+            Console.WriteLine("Please choose an option from the following:\n" +
+                            "[N]ew checksum\n" +
+                            "[R]eturn check digit\n" +
+                            "[V]alidate existing checksum\n" +
+                            "[Q]uit");
+        }
+
+        private enum ValidationOption {
             NotChosen,
             NewChecksum,
             ReturnCheckDigit,
             ValidateExistingChecksum,
         }
         
-        private ValidationType SetValidationType(string option) {
-            ValidationType validationType;
-            if (option == "N" || option == "n" ) { validationType = ValidationType.NewChecksum; }
-            else if (option == "R" || option == "r") { validationType = ValidationType.ReturnCheckDigit; }
-            else if (option == "V" || option == "v") { validationType = ValidationType.ValidateExistingChecksum; }
-            else { validationType = ValidationType.NotChosen; }
-            return validationType;
+        private ValidationOption SetValidationOption(in string option) {
+            ValidationOption validationOption = ValidationOption.NotChosen;
+            if (option == "N" || option == "n" ) 
+                { validationOption = ValidationOption.NewChecksum; }
+            else if (option == "R" || option == "r") 
+                { validationOption = ValidationOption.ReturnCheckDigit; }
+            else if (option == "V" || option == "v") 
+                { validationOption = ValidationOption.ValidateExistingChecksum; }
+            else if ( option == "Q" || option == "q" )
+                { System.Environment.Exit(0); }
+            return validationOption;
         }
 
-        private string SetUserPrompt(ValidationType validationType) {
+        private string SetUserPrompt(in ValidationOption validationOption) {
             string userMessage;
-            if (validationType == ValidationType.NewChecksum) { userMessage = "Please enter number requiring new checksum:"; }
-            else if (validationType == ValidationType.ReturnCheckDigit) { userMessage = "Please enter number for check digit:"; }
-            else if (validationType == ValidationType.ValidateExistingChecksum) { userMessage = "Please enter number requiring checksum validation:"; }
-            else { userMessage = "No valid option selected."; }
+            if (validationOption == ValidationOption.NewChecksum) 
+                { userMessage = "Please enter number requiring new checksum:"; }
+            else if (validationOption == ValidationOption.ReturnCheckDigit) 
+                { userMessage = "Please enter number for check digit:"; }
+            else if (validationOption == ValidationOption.ValidateExistingChecksum) 
+                { userMessage = "Please enter number requiring checksum validation:"; }
+            else 
+                { userMessage = "No valid option selected."; }
             return userMessage;
         }
 
-        private string TakeInput(string userMessage) {
+        private string TakeInput(in string userMessage) {
             Console.WriteLine($"{userMessage}");
             string input = Console.ReadLine();
             return input;
         }
 
-        private void RunValidation(ValidationType validationType, string input) {
-            if (validationType == ValidationType.NewChecksum) { Console.WriteLine("New checksum is " + luhnValidator.AddLuhnSuffix(input)); }
-            else if (validationType == ValidationType.ReturnCheckDigit) { Console.WriteLine($"The check digit for {input} is " + luhnValidator.ReturnLuhnSuffix(input)); }
-            else if (validationType == ValidationType.ValidateExistingChecksum) { Console.WriteLine($"Checksum is " + luhnValidator.CheckLuhnSuffixReturnString(input) + "."); }
+        private void RunValidation(in ValidationOption validationOption, string input) {
+            if (validationOption == ValidationOption.NewChecksum) 
+                { Console.WriteLine("New checksum is " + luhnValidator.AddLuhnSuffix(input)); }
+            else if (validationOption == ValidationOption.ReturnCheckDigit)
+                { Console.WriteLine($"The check digit for {input} is " + luhnValidator.ReturnLuhnSuffix(input)); }
+            else if (validationOption == ValidationOption.ValidateExistingChecksum)
+                { Console.WriteLine($"Checksum is " + luhnValidator.CheckLuhnSuffixReturnString(input) + "."); }
         }
     }
 }
